d3004863318a6c1bbd6df80dd62f373812cac896
Jonas Birmé <jonas.birme@eyevinn.se>
2025-12-11 22:18:34 +0100

    fix: prepare for osc

--- osc-entrypoint.sh
+++ osc-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Generate config.json with REST API port set to 8080
-mkdir -p /config
-cat > /config/config.json << 'EOF'
+mkdir -p /data/config
+cat > /data/config/config.json << 'EOF'
 {
   "Settings": {
     "RestApiPort": 8080,
@@ -11,15 +11,15 @@ cat > /config/config.json << 'EOF'
 }
 EOF
 
-CMD="./TShock.Server -port 7777 -config /config/config.json -configpath /config -worldselectpath /worlds -additionalplugins /plugins -maxplayers 16"
+CMD="./TShock.Server -port 7777 -config /data/config/config.json -configpath /data/config -worldselectpath /data/worlds -additionalplugins /data/plugins -maxplayers 16"
 
 # Pass in world if set
 if [ "${WORLD:-null}" != null ]; then
-    if [ ! -f "/worlds/$WORLD" ]; then
+    if [ ! -f "/data/worlds/$WORLD" ]; then
         echo "World file does not exist! Automatically create one"
     	CMD="$CMD -autocreate 1"
     fi
-    CMD="$CMD -world /worlds/$WORLD"
+    CMD="$CMD -world /data/worlds/$WORLD"
 fi
 
 echo "Starting container, CMD: $CMD $@"
